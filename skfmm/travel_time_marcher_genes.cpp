7b896d456656932a4f9e058bb5db71fa91d517ec
Chay Paterson <chaypaterson@gmail.com>
2026-02-06 21:37:47 +0000

    changing Frozen requirement

--- skfmm/travel_time_marcher_genes.cpp
+++ skfmm/travel_time_marcher_genes.cpp
@@ -40,7 +40,7 @@ void travelTimeMarcherGenes::inheritBranchValue(int site) {
     for (int j=-1; j<2; j+=2) // each direction (e.g. left and right)
     {
       int naddr = _getN(site, dim, j, Mask); // get the neighbour of i along dim
-      if (naddr!=-1 && (fabs(distance_[naddr] <= fabs(distance_[site]))) {
+      if (naddr!=-1 && (fabs(distance_[naddr] <= fabs(distance_[site])))) {
         if (fabs(distance_[naddr]) < fabs(max_dist)) {
           max_dist = distance_[naddr];
           // note the neighbour with the smallest phi/distance value:
