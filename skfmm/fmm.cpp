6540c512923f4e97f6150d981e71a376ac2d39ae
Chay Paterson <chay.paterson@manchester.ac.uk>
2026-02-06 14:52:16 +0000

    correct type for gene_marcher so we can access branch_

--- skfmm/fmm.cpp
+++ skfmm/fmm.cpp
@@ -386,7 +386,9 @@ static PyObject* distance_method(PyObject* self, PyObject* args)
       marcher->march();
       error = marcher->getError();
       if (mode == TRAVEL_TIME_GENES) {
-        branch = ((travelTimeMarcherGenes*)marcher)->branch_;
+        travelTimeMarcherGenes *gene_marcher = (travelTimeMarcherGenes*)marcher;
+        branch = (PyArrayObject *)PyArray_FROMANY(gene_marcher->branch_, 
+                                                  NPY_UINT, 0 0, NPY_IN_ARRAY);
       }
       delete marcher;
     } catch (const std::exception& exn) {
