98c4291f5b9eecd4030ec26c2816f6b2024e99d5
Chay Paterson <chaypaterson@gmail.com>
2026-03-10 14:26:16 +0000

    debug prints

--- skfmm/travel_time_marcher_genes.cpp
+++ skfmm/travel_time_marcher_genes.cpp
@@ -145,8 +145,8 @@ double travelTimeMarcherGenes::updatePointOrderTwo(int i, std::set<int>avoid_dim
   }
 
   if (branch_values.size() > 1) {
-    std::cout << branch_values << std::endl;
     for (auto& branch : branch_values) {
+      std::cout << branch << std::endl;
       std::cout << tau_values[branch] << std::endl;
     }
   }
