78fb26cc91f96b11d3b4429ce62ec73119bb5f3a
Chay Paterson <chay.paterson@manchester.ac.uk>
2026-05-21 12:21:34 +0100

    maze fixesa

--- examples/maze-solver.py
+++ examples/maze-solver.py
@@ -5,29 +5,15 @@ from PIL import Image
 import io
 from matplotlib import cm
 
-
-def add_maze_wall(a,b,speeds,taps,epsilon):
-	dsq_ab = (a[0]-b[0])**2 + (a[1]-b[1])**2
-	wall_points = []
-	for x in range(taps):
-		for y in range(taps):
-			if (x-a[0])**2 + (y-a[1])**2 + (x-b[0])**2 + (y-b[1])**2 - dsq_ab < epsilon:
-				wall_points.append((x,y))
-
-	for (x,y) in wall_points:
-		for i in range(len(speeds)):
-			speeds[i][x][y] = 0
-
-	return speeds
-
 # resolution of grid for plots:
 taps = 1000
 x_width = 2.0
 y_width = 2.0
 
 plt.figure()
+# NB: origin (0,0) should be in the centre of the space:
 X, Y = np.meshgrid(np.linspace(-0.5 * x_width, +0.5 * x_width, taps + 1), 
-                   np.linspace(-0.5 * y_width, +0.5 * y_width, taps + 1))
+				   np.linspace(-0.5 * y_width, +0.5 * y_width, taps + 1))
 phi = (Y)**2+(X)**2
 drivers = {1: [0.5, 0], 2: [-0.2,0.3], 4: [-0.4,0.6]} # a dictionary with n entries
 speeds = [1+X**4, 4+X**4, 3+X**4, 7*X**4,2+X**4,3+X**4,4+X**4,5+X**4] # a list of 2^n speed functions
@@ -38,11 +24,37 @@ print(drivers)
 # add white noise to speeds:
 sigma = 0.1 # noise loudness
 speeds = [np.random.normal(speed, sigma) for speed in speeds]
+
+# build a maze:
+def dist(a, b):
+   # return distance between two (real) points on a plane (2D):
+   return np.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
+
+def add_maze_wall(a, b, speeds, X_mesh, Y_mesh, epsilon):
+	dsq_ab = dist(a, b)
+	dx = (X_mesh[0][1] - X_mesh[0][0])
+	x_taps = X_mesh.shape[0]
+	y_taps = X_mesh.shape[1]
+	wall_points = []
+	for x, y in np.nditer([X_mesh, Y_mesh]):
+		if ((dist((x,y), a) + dist((x,y), b)) < (np.sqrt(dsq_ab) + epsilon)):
+			wall_points.append((x,y))
+
+	for (x,y) in wall_points:
+		for i in range(len(speeds)):
+			row = int(x / dx + 0.5 * x_taps) % x_taps
+			col = int(y / dx + 0.5 * y_taps) % y_taps
+			speeds[i][row][col] = 0
+
+	return speeds
+
+speeds = add_maze_wall((0.3,0.3),(0.3,0.6),speeds,X, Y, -0.15)
+
+# solve the system:
 tau, bfield = skfmm.travel_time_genes(phi, drivers, speeds, dx=x_width/taps)
 bfield_max = bfield.max()
 
-speeds = add_maze_wall((taps*0.3,taps*0.3),(taps*0.3,taps*0.6),speeds,taps,100)
-
+#plot the solution:
 plt.figure()
 num_frames = 50
 time_min = 0
